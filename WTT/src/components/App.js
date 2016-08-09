1e9947d06ecce5ecbe83da88771ce0a86a51d6e1
joseph sul <joesul7@gmail.com>
2016-08-09 16:34:59 -0400

    changes

--- WTT/src/components/App.js
+++ WTT/src/components/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import '../stylesheets/App.css';
 import axios from '../utils/helper.js';
-<<<<<<< HEAD
-=======
-
->>>>>>> f3e7d209bd9c8e3c2429806582c56091441859df
 
 class App extends Component {
 
