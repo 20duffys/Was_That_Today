a946871a5caecba71cb59839c42ed8cb633cc5b9
joseph sul <joesul7@gmail.com>
2016-08-09 17:32:04 -0400

    commit to pull

--- WTT/src/index.js
+++ WTT/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Header from './components/Header';
 import './stylesheets/index.css';
 import {Router, Route, browserHistory} from 'react-router';
 
