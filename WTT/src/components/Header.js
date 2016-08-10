b39cdd53a19cd615bfc7457dc24e29a5b2b2d80e
Austin Hudson <arhudson7@gmail.com>
2016-08-09 22:00:19 -0400

    used Header to incorporate Login Component

--- WTT/src/components/Header.js
+++ WTT/src/components/Header.js
@@ -1,11 +1,17 @@
 /*
-Written by Joe
+Written by Joe, edited by Austin
 */
 
 import React, { Component } from 'react';
+import {browserHistory} from 'react-router'
 import '../stylesheets/Header.css';
 
 class Header extends Component {
+
+  handleLogin(){
+    browserHistory.push("/login");
+  }
+
   render(){
     return(
       <div>
@@ -14,7 +20,7 @@ class Header extends Component {
             <img className='logo' height="45" width="45" role="presentation" src='gift.png'/>
           </button>
           <h1>Was That Today?</h1>
-          <button className='loginButton'>Login</button>
+          <button className="loginButton" onClick={this.handleLogin.bind(this)}>Login</button>
         </header>
       </div>
     )
