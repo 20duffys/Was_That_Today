8eee8e0dcd51d01c8a781cd5bdb449e592275b07
joseph sul <joesul7@gmail.com>
2016-08-10 10:51:20 -0400

    adding to Search.js

--- WTT/src/components/Search.js
+++ WTT/src/components/Search.js
@@ -2,21 +2,39 @@ import React, {Component} from 'react'
 import axios from '../utils/helper.js'
 
 /*
-  Written by Austin
+  Written by Austin, edited by Joe
 */
+
 class Search extends Component {
 
-  productSearch(event){
+  constructor(props) {
+    super(props) {
+      this.state = {
+        itemImgUrl: "",
+        itemName: "",
+        itemPrice: "",
+        itemBuyUrl: "",
+        responseData: []
+      }
+    }
+  }
+
+  productSearch(event) {
     event.preventDefault();
     let searchItem = event.target.elements[0].value;
     if(searchItem !== null || searchItem.length !== 0){
       axios.search(searchItem).then((res) => {
-          console.log(res.data);
+        this.setState({
+          responseData: res.data
+        })
+        console.log(res.data);
       })
     }
   }
 
-  render(){
+  render() {
+    const products = this.state.reponse;
+
     return(
       <div>
         <form className="search-form" onSubmit={(event) => this.productSearch(event)}>
