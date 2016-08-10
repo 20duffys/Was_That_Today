0797e6af1cea5b6a46bba03f645d9f0f9c171d1a
Austin Hudson <arhudson7@gmail.com>
2016-08-10 09:55:02 -0400

    added search component so it's more dynamic

--- WTT/src/components/App.js
+++ WTT/src/components/App.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
 import '../stylesheets/App.css';
-import axios from '../utils/helper.js'
 import {Link} from 'react-router'
 
 
 class App extends Component {
 
-
-  productSearch(){
-      axios.search().then((res) => {
-        console.log(res.data);
-      })
-
-  }
-
   render() {
     return (
       <div className="App">
-        <button onClick={this.productSearch.bind(this)}>Click me</button>
         {this.props.children}
       </div>
     );
