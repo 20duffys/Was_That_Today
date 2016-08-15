de4004504688851563003d9d686173925cbf1417
Sam Duffy <[Asduffy20@stanford.edu>
2016-08-14 23:10:58 -0400

    finalizing ReadMe and About

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# Was_That_Today
-A website which allows you to buy gifts on the fly in your area.
+# Was That Today?
+Austin Hudson
+Joseph Sul
+Sam Duffy
+
+### Description
+This is a full-stack web app using a 3rd party web storage system and uses an API.
+
+In this app, the client can log into his own account and look for any type of gift available on the web. Once the client has found an item that he likes, he can either click on one of the provided links to access a site selling that item, or he can add that item to a list of favorite items. Once the client accesses the list of searched items, he can also save a specific event name and date to the item. If he doesn't like a specific item, he can delete that item from the list.
+
+### Work Distribution
+While we initially split up into doing separate assignments, (Austin completing the login page, Joe on styling, and Sam on the About page) we ended up primarily working together on one laptop to save time on git push/branch errors. This way, we were able to find bugs faster and solve problems at a faster rate.
+
+##Technology Used
+ - Javascript
+ - HTML
+ - CSS
+ - React
+ - React-Router
+ - scripts
+ - body-parser
+ - Cors
+ - Nodemon
+ - express
+ - node
+ - Firebase DB
+ - Bootstrap
+
+##How it works
+
+#### API Call/ViewAll:
+The first section of the app which we completed was the API call. The API that we used in this project was Semantics3. Semantics3 utilizes over a dozen different websites to look for items. In order to call this API, we first studied Semantics3's documentation. After studying the API, we rendered specific information including the item name, image, price, and sites where the item is sold. Each item was individually rendered onto the page. Each item has a button which allows the client to "favorite" that item.
+
+For this section we used the following files:
+ItemCard.js
+Search.js
+helper.js
+
+
+#### Save to Firebase:
+The next major challenge for our project was creating a database using firebase. We opted to make our own custom database. In this section we began to focus on the username and password login system as well. We initially difficulty to organize the firebase dataset using unique keys while also not nesting data in a way which was difficult to re-render.
+
+For this section we used the following files:
+firebase.js
+Favorite.js
+Login.js
+LoginHeader.js
+UserPanel.js
+NewUser.js
+
+
+#### View Favorites page
+The favorites page was perhaps the hardest part of the project. We had to display the information from the firebase database onto the page in a modal. In order to display the information, we used react-router and set a wide number of different parameters in order to grab the date properly depending on the client's username, password, and selected items. Then, we added the functionality to add an event name and date directly to Firebase.
+
+For this section we used the following files:
+DeleteItem.js
+Events.js
+firebase.js
+App.js
+GiftList.js
+
+##Unresolved Problems
+  Nothing so far.
+
+##Link:
+https://peaceful-beach-68189.herokuapp.com/home
--- WTT/src/components/About.js
+++ WTT/src/components/About.js
@@ -12,7 +12,7 @@ class About extends Component {
               <h3>Austin Hudson</h3>
                 <a href="https://www.linkedin.com/in/austin-hudson" target="_blank">Linkedin</a>
                 <a href="https://github.com/Austin-Hudson" target="_blank">GitHub</a>
-              <p>Austin is a full stack Web Developer and soon to be graduate of the prestigious General Assembly Web Development immersive. Coming from an IT technician background, Austin brings precison and ingenuity to the web development field. Austin is also a Stony Brook graduate and his previous project includes GoodEats, a site which allows one to upload photos and comment on restaurants</p>
+              <p>Austin is a full stack Web Developer and soon to be graduate of the prestigious General Assembly Web Development Immersive. As a former IT technician, Austin brings precison and ingenuity to the web development field. Austin holds a computer science degree from SUNY Stony Brook, and his previous project includes GoodEats, a site which allows one to upload photos and comment on restaurants.</p>
             </div>
           </div>
           <br />
@@ -22,7 +22,7 @@ class About extends Component {
               <h3>Joseph Sul</h3>
                 <a href="https://www.linkedin.com/in/joesul" target="_blank">Linkedin</a>
                 <a href="https://github.com/joesul" target="_blank">GitHub</a>
-              <p>A former general manager at NYU Bakery INC, Joseph has taken on the challenge of becoming a web developer with stride, illustrating a level head and an adaptive mind. Early in the project, Joseph was quick to use his soft skills to persuade the company behind this project's API that "Was That Today?" deserved to have access to the entire API rather than a small snipit. Joseph is a great manager wherever he goes.</p>
+              <p>With strong managerial experience at New York based retail companies, Joseph is an effective web developer who can solve problems with confidence. He is level headed and adaptive. In the early stages of the company, Joseph persuaded Semantics3 that the company "Was That Today?" should have access to the entire API database rather than a sample segment. Joseph is a great manager.</p>
             </div>
         </div>
           <br />
@@ -32,7 +32,7 @@ class About extends Component {
               <h3>Sam Duffy</h3>
                 <a href="https://www.linkedin.com/in/samduffycodes" target="_blank">Linkedin</a>
                 <a href="https://github.com/20duffys" target="_blank">GitHub</a>
-              <p>Sam is the youngest person to ever be in the General Assembly Web Development Immersive. Only a recent high-school graduate, Sam decided to thrust himself into the coding world before going to Stanford. Despite never coding before two weeks before the immersive, he has driven himself to the challenge.
+              <p>Sam is the youngest person to ever be admitted to the General Assembly Web Development Immersive. A recent high-school graduate of the Collegiate School, which has a strong focus in the humanities, Sam decided to learn coding before going to Stanford in the Fall. A published author and winner of national history prizes, Sam sees a strong connection between writing and coding. He loves crushing bugs.
             </p>
             </div>
           </div>
